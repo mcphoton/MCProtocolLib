4e5c79c3efbdb9d4520bde37158235fe3cbffde3
TheElectronWill <theelectronwill@gmail.com>
2017-05-17 11:56:51 +0200

    Effect packet: remove unused imports

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/server/world/ServerPlayEffectPacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/server/world/ServerPlayEffectPacket.java
@@ -1,16 +1,8 @@
 package com.github.steveice10.mc.protocol.packet.ingame.server.world;
 
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.IntPosition;
-import com.github.steveice10.mc.protocol.data.game.world.effect.SmokeEffectData;
-import com.github.steveice10.mc.protocol.data.game.world.effect.SoundEffect;
 import com.github.steveice10.mc.protocol.data.game.world.effect.WorldEffectData;
 import com.github.steveice10.mc.protocol.data.MagicValues;
-import com.github.steveice10.mc.protocol.data.game.world.block.BlockState;
-import com.github.steveice10.mc.protocol.data.game.world.effect.BonemealGrowEffectData;
-import com.github.steveice10.mc.protocol.data.game.world.effect.BreakBlockEffectData;
-import com.github.steveice10.mc.protocol.data.game.world.effect.BreakPotionEffectData;
-import com.github.steveice10.mc.protocol.data.game.world.effect.ParticleEffect;
-import com.github.steveice10.mc.protocol.data.game.world.effect.RecordEffectData;
 import com.github.steveice10.mc.protocol.data.game.world.effect.WorldEffect;
 import com.github.steveice10.mc.protocol.util.NetUtil;
 import com.github.steveice10.mc.protocol.util.ReflectionToString;
@@ -27,8 +19,7 @@ public final class ServerPlayEffectPacket implements Packet {
     private boolean broadcast;
 
     @SuppressWarnings("unused")
-    private ServerPlayEffectPacket() {
-    }
+    private ServerPlayEffectPacket() {}
 
     public ServerPlayEffectPacket(WorldEffect effect, IntPosition position, WorldEffectData data) {
         this(effect, position, data, false);
